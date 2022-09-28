18535594cb66a787bb5c5723ecb29ed16cccb062
heno <ngvanchinh007@gmail.com>
2022-09-28 18:12:11 +0700

    test fix upload 1

--- src/uploads/uploadReviewImages.js
+++ src/uploads/uploadReviewImages.js
@@ -4,8 +4,21 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "images_store/");
-    console.log(path.join(process.cwd(), "review_images_store"));
-    cb(null, path.join(process.cwd(), "review_images_store"));
+    // console.log(path.join(process.cwd(), ".."));
+    // console.log(__dirname);
+    // let pathStore = path.join(process.cwd(), "review_images_store");
+    // let pathStore = path.join(process.cwd(), "..", "review_images_store");
+    // console.log(pathStore);
+    if (process.env.NODE_ENV === "production") {
+      pathStore = path.join(
+        path.join(process.cwd(), "..", "review_images_store")
+      );
+      console.log(pathStore);
+      cb(null, pathStore);
+    } else {
+      console.log(path.join(process.cwd(), "review_images_store"));
+      cb(null, path.join(process.cwd(), "review_images_store"));
+    }
   },
   filename: function (req, file, cb) {
     cb(
